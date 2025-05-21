4433155ec85a40ce26ef763159ada6369c5770a7
aquoc09 <anhquoc6a4@gmail.com>
2025-05-22 01:32:16 +0700

    update register

--- js/login.js
+++ js/login.js
@@ -9,7 +9,7 @@ function loadAccountData() {
         });
 }
 
-function verify() {
+function verifyLogin() {
     document.getElementById("loginForm").addEventListener("submit", function (e) {
         e.preventDefault();
         var username = document.getElementById('username').value.trim();
@@ -64,6 +64,75 @@ function verify() {
                 title.innerHTML = 'Sai tài khoản hoặc mật khẩu';
             }
         }
+    });
+}
+
+function verifyRegister() {
+    document.getElementById("registerForm").addEventListener("submit", function (e) {
+        e.preventDefault();
+        var username = document.getElementById('username').value.trim();
+        var password = document.getElementById('password').value.trim();
+        var confirmPassword = document.getElementById('confirmPassword').value.trim();
+        var fullName = document.getElementById('fullName').value.trim();
+        var phone = document.getElementById('phone').value.trim();
+
+        var usernameErrorMessage = document.getElementById('usernameErrorMessage');
+        var passwordErrorMessage = document.getElementById('passwordErrorMessage');
+        var confirmPasswordErrorMessage = document.getElementById('confirmPasswordErrorMessage');
+        var fullNameErrorMessage = document.getElementById('fullNameErrorMessage');
+        var phoneErrorMessage = document.getElementById('phoneErrorMessage');
+
+        var userRegex = /^[0-9a-zA-Z]{4,}$/;
+        var passwordRegex = /^.{4,}$/;
+        var fullNameRegex = /[a-zA-Z]/;
+        var phoneRegex = /^[0-9]{10,}$/;
+
+        var isValid = true;
+
+        if (!userRegex.test(username)) {
+            usernameErrorMessage.innerHTML = 'Tài khoản không được có kí tự đặc biệt!';
+            isValid = false;
+        } else {
+            usernameErrorMessage.innerHTML = '';
+        }
+
+        if (!passwordRegex.test(password)) {
+            passwordErrorMessage.innerHTML = 'Mật khẩu phải dài hơn 4 kí tự!';
+            isValid = false;
+        } else {
+            passwordErrorMessage.innerHTML = '';
+        }
+
+        if (!passwordRegex.test(confirmPassword)) {
+            confirmPasswordErrorMessage.innerHTML = 'Mật khẩu phải dài hơn 4 kí tự!';
+            isValid = false;
+        } else {
+            confirmPasswordErrorMessage.innerHTML = '';
+        }
+
+        if (!fullNameRegex.test(fullName)) {
+            fullNameErrorMessage.innerHTML = 'Tên không hợp lệ';
+            isValid = false;
+        } else {
+            fullNameErrorMessage.innerHTML = '';
+        }
+
+        if (!phoneRegex.test(phone)) {
+            phoneErrorMessage.innerHTML = 'SĐT phải có đủ 10 chữ số!';
+            isValid = false;
+        } else {
+            phoneErrorMessage.innerHTML = '';
+        }
+
+        if (isValid) {
+            //Hiển thị modal thông báo đã đăng kí thành công
+            const btn_modal = document.getElementById('btnModal');
+            btn_modal.click();
+            const title = document.getElementById('title-text-modal');
+
+            title.innerHTML = 'Đã đăng kí thành công';
+        }
+        this.reset()
     });
 }
 
